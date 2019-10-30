ce1bb87119cd9d7bb50a18631e682b96bd12e1c6
ouweiqi <ouweiqi@jd.com>
2019-10-30 09:59:38 +0800

    添加注释

--- jstorm-core/src/main/java/com/alibaba/jstorm/callback/AsyncLoopThread.java
+++ jstorm-core/src/main/java/com/alibaba/jstorm/callback/AsyncLoopThread.java
@@ -31,13 +31,22 @@ import com.alibaba.jstorm.utils.SmartThread;
 /**
  * Wrapper Timer thread Every several seconds execute afn, if something is run, run kill_fn
  *
+ * AsyncLoopThread 类封装的是一个每隔一段时间执行一次的主任务，同时设置一个配套的监督任务，用于在主任务发生异常时，中断执行线程。所以该类当做一个通用的简易定时任务类使用
+ *
+ *
  * @author yannian
  */
 public class AsyncLoopThread implements SmartThread {
     private static final Logger LOG = LoggerFactory.getLogger(AsyncLoopThread.class);
 
+    /**
+     * 每隔一段时间执行一次的主任务
+     */
     private Thread thread;
 
+    /**
+     *每隔一段时间执行一次的主任务
+     * */
     private RunnableCallback afn;
 
     public AsyncLoopThread(RunnableCallback afn) {
@@ -57,11 +66,22 @@ public class AsyncLoopThread implements SmartThread {
         this.init(afn, daemon, kill_fn, priority, start);
     }
 
+    /**
+     * param :
+     *  afn 需要使用线程异步循环往复执行的任务
+     *  daemon 是否作为守护线程执行，默认值为 false
+     *  priority 线程的优先级，默认值为 Thread.NORM_PRIORITY
+     *  start 是否立刻启动该线程，默认值为 true
+     *  kill_fn 指定执行杀死任务的任务，默认值为 AsyncLoopDefaultKill 实例
+     * */
     private void init(RunnableCallback afn, boolean daemon, RunnableCallback kill_fn, int priority, boolean start) {
         if (kill_fn == null) {
             kill_fn = new AsyncLoopDefaultKill();
         }
 
+        /**
+         * AsyncLoopRunnable 对象，它组合了要执行的主任务和负责杀死主任务的任务，作为一个组合任务
+         * */
         Runnable runnable = new AsyncLoopRunnable(afn, kill_fn);
         thread = new Thread(runnable);
         String threadName = afn.getThreadName();
