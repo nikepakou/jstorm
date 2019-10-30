cded35864789b2fcf44e2a9fcff73c442c014f7c
ouweiqi <ouweiqi@jd.com>
2019-10-30 09:57:21 +0800

    添加注释

--- jstorm-core/src/main/java/backtype/storm/task/IOutputCollector.java
+++ jstorm-core/src/main/java/backtype/storm/task/IOutputCollector.java
@@ -24,9 +24,22 @@ import java.util.List;
 public interface IOutputCollector extends IErrorReporter {
     /**
      * Returns the task ids that received the tuples.
+     *
+     */
+    /**
+     *  用来向外发送数据,它的返回值是该消息所有发送目标的TaskId集合
+     *  输入参数:
+     *  streamId:消息将被输出到的流;
+     *  anchors:输出消息的标记,通常代表该条消息是由哪些消息产生的,主要用于消息的Ack系统;
+     *  tuple:要输出的消息,为一个Object列表
      */
     List<Integer> emit(String streamId, Collection<Tuple> anchors, List<Object> tuple);
 
+    /**
+     * 与上述方法emit类似,区别在于,emitDirect方法发送的消息只有指定taskId的Task才可以接收.
+     * 该方法要求streamId对应的流必须为直接流而且接收端的Task必须通过直接分组的方式来接收消息,否则会抛出异常.
+     * 这也就意味着:如果没有下游节点接收该消息,则此类消息其实并没有真正被发送
+     */
     void emitDirect(int taskId, String streamId, Collection<Tuple> anchors, List<Object> tuple);
 
     void ack(Tuple input);
